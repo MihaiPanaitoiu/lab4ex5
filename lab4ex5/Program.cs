fcca92e8982ba50ea9226dad3d115cf56e17664b
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2023-01-10 19:54:55 +0200

    Add project files.

---
+++ lab4ex5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab4ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Scrieti o functie recursiva care va calcula suma numerelor de la 1 pana la n,
+             apelati-o si afisati-i rezultatul
+             */
+            Console.WriteLine("Introduceti n");
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetSumFromOneToN(n));
+        }
+
+        static int GetSumFromOneToN(int n)
+        {
+            int sum = 0;
+            if (n > 0)
+            {
+                sum += n + GetSumFromOneToN(n - 1);
+                return sum;
+            }
+            return sum;
+        }
+    }
+}
